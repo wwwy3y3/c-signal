9b391d77c4c2a7e8fbb8016a3179ebc5a5bcf63e
william <wwwy3y3@gmail.com>
2013-12-30 16:13:15 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -17,23 +17,24 @@
 using namespace std;
 using namespace boost::numeric::ublas;
 
-void initMatrix(matrix<int> &m){
-	for (unsigned i = 0; i < m.size1 (); ++ i)
-        for (unsigned j = 0; j < m.size2 (); ++ j)
-        	cin>> m(i,j);
+int multi1(int &m, int &n){
+	return 2+4*m*n;
 }
 
-void writeMatrix(matrix<int> &m, int* &map, int idx){
-	for (unsigned i = 0; i < m.size1 (); ++ i)
-        for (unsigned j = 0; j < m.size2 (); ++ j){
-        	map[idx]= m(i,j);
-        	idx++;
-        }
+int multi2(int &m, int &n){
+	return 2+4*m*n+m*m;
 }
 
-void handler1(int signum){
-	int idx=2;
-    int fd;
+int multi3(int &m, int &n){
+	return 2+2*m*n;
+}
+
+int multi4(int &m, int &n){
+	return 2;
+}
+
+void prodMatrix(int (*start)( int&,int& ),  int (*write)( int&,int& )){
+	int fd;
     int *map;  /* mmapped array of int's */
 
     fd = open(FILEPATH, O_RDWR, S_IRUSR | S_IWUSR);
@@ -48,13 +49,14 @@ void handler1(int signum){
 		perror("Error mmapping the file");
 		exit(EXIT_FAILURE);
     }
-
+    
     //read A1,B1
     int m= map[0];
     int n= map[1];
+    int idx= (*start)(m,n);
     matrix<int> A(m,n);
 	matrix<int> B(n,m);
- 
+
 	for (unsigned i = 0; i < A.size1 (); ++ i)
         for (unsigned j = 0; j < A.size2 (); ++ j)
         	A(i,j)= map[idx++];
@@ -66,8 +68,8 @@ void handler1(int signum){
     //write result back
     matrix<int> M(m,m);
     M= prod(A,B);
-    cout<<M;
-    idx= 26;
+    
+    idx= (*write)(m,n);
     for (unsigned i = 0; i < M.size1 (); ++ i)
         for (unsigned j = 0; j < M.size2 (); ++ j)
         	map[idx++]= M(i,j);
@@ -76,56 +78,17 @@ void handler1(int signum){
 	perror("Error un-mmapping the file");
 	/* Decide here whether to close(fd) and exit() or not. Depends... */
     }
+}
+
+void handler1(int signum){
+    prodMatrix(&multi4,&multi1);
+    cout<<"setup1"<<endl;
 	exit(1);
 }
 
 void handler2(int signum){
-	int idx=14;
-    int fd;
-    int *map;  /* mmapped array of int's */
-
-    fd = open(FILEPATH, O_RDWR, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-		perror("Error opening file for reading");
-		exit(EXIT_FAILURE);
-    }
-
-    map = (int*)mmap(0, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED) {
-		close(fd);
-		perror("Error mmapping the file");
-		exit(EXIT_FAILURE);
-    }
-    
-    //read A1,B1
-    int m= map[0];
-    int n= map[1];
-
-    matrix<int> A(m,n);
-	matrix<int> B(n,m);
-
-	for (unsigned i = 0; i < A.size1 (); ++ i)
-        for (unsigned j = 0; j < A.size2 (); ++ j)
-        	A(i,j)= map[idx++];
-
-    for (unsigned i = 0; i < B.size1 (); ++ i)
-        for (unsigned j = 0; j < B.size2 (); ++ j)
-        	B(i,j)= map[idx++];
-
-    //write result back
-    matrix<int> M(m,m);
-    M= prod(A,B);
-    cout<<M;
-    
-    idx= 35;
-    for (unsigned i = 0; i < M.size1 (); ++ i)
-        for (unsigned j = 0; j < M.size2 (); ++ j)
-        	map[idx++]= M(i,j);
-
-    if (munmap(map, FILESIZE) == -1) {
-	perror("Error un-mmapping the file");
-	/* Decide here whether to close(fd) and exit() or not. Depends... */
-    }
+    prodMatrix(&multi3,&multi2);
+    cout<<"setup2"<<endl;
 	exit(1);
 }
 
@@ -135,20 +98,8 @@ int main(int argc, char const *argv[])
 	int status = 0;
 	pid_t fp1,fp2, wpid;
 	cin>>m>>n;
-	matrix<int> A1(m,n);
-	matrix<int> B1(n,m);
-	matrix<int> A2(m,n);
-	matrix<int> B2(n,m);
-	matrix<int> M(m,m);
-	matrix<int> N(m,m);
-
-	initMatrix(A1);
-	initMatrix(B1);
-	initMatrix(A2);
-	initMatrix(B2);
 
 	//mmap
-	int i;
     int fd;
     int result;
     int *map;  /* mmapped array of int's */
@@ -192,10 +143,10 @@ int main(int argc, char const *argv[])
 	 */
     map[0]= m;
     map[1]= n;
-    writeMatrix(A1,map,2);
-    writeMatrix(B1,map,8);
-    writeMatrix(A2,map,14);
-    writeMatrix(B2,map,20);
+    int tmp,i=2;
+    while(cin>>tmp){
+    	map[i++]= tmp;
+    }
 
 	//signal
 	struct sigaction sa,sa2;
@@ -229,20 +180,23 @@ int main(int argc, char const *argv[])
 
 	//kill
 	if (fp1>0 && fp2>0) //father
-	{
-		cout<<"father"<<endl;
+	{	
+		cout<<"kill"<<endl;
 		kill(fp1, SIGUSR1);
 		kill(fp2, SIGUSR2);
 	
 	//wait child
 	while ((wpid = wait(&status)) > 0)
-	    {
+	    {	
 	        printf("Exit status of %d was %d (%s)\n", (int)wpid, status,
 	               (status > 0) ? "accept" : "reject");
 	    }
 
 	//get the eventual result
-	int idx= 26;
+	matrix<int> M(m,m);
+	matrix<int> N(m,m);
+
+	int idx= 2+4*m*n;
 	for (unsigned i = 0; i < M.size1 (); ++ i)
         for (unsigned j = 0; j < M.size2 (); ++ j)
         	M(i,j)= map[idx++];
